926a5332e7cf6be96ebc7b6e254ad17a1f42dfbe
DanoBaltar <63501871+DanoBaltar@users.noreply.github.com>
2022-09-21 12:19:34 +0200

    Add files via upload
    Added to branch test

---
+++ Student_confidence_interval.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 21:01:45 2021
+
+@author: dbaltar
+"""
+
+# Calculates confidence interval using T, useful for small samples
+
+# Calculate the sample average
+# Calculate the sample standard deviation
+# Calculate the sqrt(sample size)
+# Calculate the standard error = Standard deviation / sqrt(sample size)
+# Calculate the T value for the confidence interval
+# Calculate alfa = Confidence interval - 1
+# Calculate T(samples, Alfa/2)
+# Calculate interval half-width = T Standard Error
